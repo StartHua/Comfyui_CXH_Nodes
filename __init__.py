8899d2a7126fb0cfa1f809539c67e395b9b6153d
DICTION\chenxinghua <455758525@qq.com>
2024-05-29 13:35:22 +0800

    重写Load_image节点支持MPO 格式图片

--- __init__.py
+++ __init__.py
@@ -3,16 +3,19 @@
 
 from .image_nodes import Net_image
 from .image_nodes import SubtractMask
+from .image_nodes import CLoad_Image
 from .LLM_nodes import Phi_3_vision
 
 NODE_CLASS_MAPPINGS = {
     "Net_image":Net_image,
     "SubtractMask":SubtractMask,
-    "Phi_3_vision":Phi_3_vision
+    "Phi_3_vision":Phi_3_vision,
+    "CLoad_Image":CLoad_Image
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Net_image":"Net_image",
     "SubtractMask":"SubtractMask",
-    "Phi_3_vision":"Phi_3_vision"
+    "Phi_3_vision":"Phi_3_vision",
+    "CLoad_Image":"CLoad_Image"
 }
--- image_nodes.py
+++ image_nodes.py
@@ -12,6 +12,54 @@ from PIL.PngImagePlugin import PngInfo
 from .lib.ximg import *
 import cv2
 
+#  SyntaxError: not a JPEG file 解决
+class CLoad_Image:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {
+            "required": {
+                "image":(sorted(files), {"image_upload": True}), #image_upload显示 
+            }
+        }
+
+    CATEGORY = "CXH/images"
+    RETURN_TYPES = ("IMAGE", "MASK")
+    FUNCTION = "load_image"
+    def load_image(self,image):
+        output_images = []
+        output_masks = []
+        
+        image_path = folder_paths.get_annotated_filepath(image)
+        img = open_image(image_path)  
+        # 遇到MPO格式图片加载报错:  
+        if img.format == "MPO":
+            img =  img.convert("P")
+        for i in ImageSequence.Iterator(img):
+            if i.mode == 'I':
+                i = i.point(lambda i: i * (1 / 255))
+            image = i.convert("RGB")
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+            if 'A' in i.getbands():
+                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            else:
+                mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+            output_images.append(image)
+            output_masks.append(mask.unsqueeze(0))
+
+        if len(output_images) > 1:
+            output_image = torch.cat(output_images, dim=0)
+            output_mask = torch.cat(output_masks, dim=0)
+        else:
+            output_image = output_images[0]
+            output_mask = output_masks[0]
+
+        return (output_image, output_mask)
+
+  
 
 
 class Net_image:
@@ -40,6 +88,10 @@ class Net_image:
         else:
             image_path = folder_paths.get_annotated_filepath(image)
             img = open_image(image_path)
+
+        # 遇到MPO格式图片加载报错:  
+        if img.format == "MPO":
+            img =  img.convert("P")
 
         for i in ImageSequence.Iterator(img):
             # i = open_image(i)
